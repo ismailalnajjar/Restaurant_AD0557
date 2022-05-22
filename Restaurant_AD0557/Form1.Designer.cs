65f1196079cecadca8a27339cadafbbcc1988634
Ismail <ismaillalnajjaar@gmail.com>
2022-05-22 22:18:56 +0300

    add database

--- Restaurant_AD0557/Form1.Designer.cs
+++ Restaurant_AD0557/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace Restaurant_AD0557
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnLogin
@@ -106,11 +107,20 @@ namespace Restaurant_AD0557
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 8;
+            // 
             // RestaurantAcc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUserName);
@@ -135,6 +145,7 @@ namespace Restaurant_AD0557
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
     }
 }
 
